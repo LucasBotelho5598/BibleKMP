e2eebd314b43ad6584da43cfad24ab7317a18769
LucasBotelho5598 <lucasAlves5598@gmail.com>
2026-09-19 11:03:36 -0300

    "Deus é Fiel"

--- shared/src/androidMain/kotlin/com/example/biblekmp/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/example/biblekmp/Platform.android.kt
@@ -6,6 +6,3 @@ class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
-actual fun getPlatform(): io.ktor.util.Platform {
-    TODO("Not yet implemented")
-}
